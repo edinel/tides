05b29b3c03d7b04e61b070d01f0aafbc7ed67ba4
Eddie Dinel <edinel@solace.org>
2023-03-19 17:23:58 -0700

    Test

--- ec2-startup.py
+++ ec2-startup.py
@@ -1,74 +1,35 @@
 #!/usr/bin/env python3
 import boto3
-#import pprint
+import pprint
+pp=pprint.PrettyPrinter(indent=1)
 
-ec2 = boto3.client('ec2')
-response = ec2.describe_regions()
-active_instances = []
-inactive_instances = []
+ec2_us_west_2 = boto3.client('ec2', region_name='us-west-2')
+
+template_name='tide_web_service'
+instance_spec = {
+    'KeyName': 'tides',
+    'MaxCount': 1,
+    'MinCount': 1,
+    'LaunchTemplate': {
+        'LaunchTemplateName': template_name,
+        'Version': '$Latest'
+    }
+}
 
 
- 
+print ("Instance Spec\n")
+pp.pprint(instance_spec)
+
+tide_instance = ec2_us_west_2.run_instances (**instance_spec);
+
+print ("\n\n")
+print ("Returned Instance\n")
+pp.pprint (tide_instance)
+
 #print (active_instances)       
-print('Active Instances:')
-for x in active_instances:
-    print (x)
-print ('Inactive Instances:')
-for x in inactive_instances:
-    print (x)
-
-
-
-    #active_instances.extend([i['Instances'][0]['InstanceId'] for i in new_client.describe_instances() ['Reservations'] if i['Instances'][0]['State']['Code'] == 16]) #parse through a mile of dict to find the instance ID 
-    
-    #print ([i['Instances'][0]['InstanceId'] for i in new_client.describe_instances() ['Reservations'] if i['Instances'][0]['State']['Code'] == 16]) #parse through a mile of dict to find the instance ID
-    #inactive_instances.extend([i['Instances'][0]['InstanceId'] for i in new_client.describe_instances() ['Reservations'] if i['Instances'][0]['State']['Code'] != 16]) #parse through a mile of dict to find the instance ID
-    
-    
-       
-#
-#for y in inactive_instances:
-#    print ("inactive", y)
-
-
-#ec2_west_2 = boto3.client('ec2', region_name="us-west-2") #Get a client for each region
-#ec2_east_2 = boto3.client('ec2', region_name="us-east-2") #Get a client for each region
-
-#active_instances_west_2 = [i['Instances'][0]['InstanceId'] for i in ec2_west_2.describe_instances() ['Reservations'] if i['Instances'][0]['State']['Code'] == 16] #parse through a mile of dict to find the instance ID
-#active_instances_east_2 = [i['Instances'][0]['InstanceId'] for i in ec2_east_2.describe_instances() ['Reservations'] if i['Instances'][0]['State']['Code'] == 16] #parse a mile of dict to find the instance ID
-
-#inactive_instances_west_2 = [i['Instances'][0]['InstanceId'] for i in ec2_west_2.describe_instances() ['Reservations'] if i['Instances'][0]['State']['Code'] != 16]
-#inactive_instances_east_2 = [i['Instances'][0]['InstanceId'] for i in ec2_east_2.describe_instances() ['Reservations'] if i['Instances'][0]['State']['Code'] != 16]
-
-#active_instances = active_instances_west_2
-#active_instances.extend(active_instances_east_2) #if you append() you end up adding the array object into the array, which isn't what you want.
-#inactive_instances = inactive_instances_east_2
-#inactive_instances.extend(inactive_instances_west_2)
-
-
-#instance_ids = [i['Instances'][0]['InstanceId'] for i in response_dict["Reservations"]]
-#for x in instance_ids:
+#print('Active Instances:')
+#for x in active_instances:
 #    print (x)
-#
-
-#response_str = str(response_dict)
-#response_json = json.loads (response_str)
-#json_response = json.dumps(response, indent=2)
-#print(json_response)
-
-
-
-#pp=pprint.PrettyPrinter(indent=1)
-#pp.pprint(response_dict)
-#print ("\n\n\n")
-
-#pp.pprint(response_dict['Reservations'][0]['Instances'][0]['InstanceId'])
-#print(response_dict['Reservations'][0]['Instances'][0]['InstanceId'])
-
-
-#for i in response_dict ['Reservations']:
-#    if i['Instances'][0]['State']['Code'] == 16:
-#        print ("running:", i['Instances'][0]['InstanceId'])
-#    else:
-#        print ("not running:",i['Instances'][0]['InstanceId'])
-#print ("done double looping")
+#print ('Inactive Instances:')
+#for x in inactive_instances:
+#    print (x)
